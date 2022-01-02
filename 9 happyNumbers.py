3f5cdd2b1743d55d16a2bf144c4c399eed8038ab
Charybdis <90869683+Charybdis2@users.noreply.github.com>
2022-01-02 12:31:38 +0000

    Add files via upload

---
+++ 9 happyNumbers.py
@@ -0,0 +1,15 @@
+def is_happy_number(number):
+    """Prints if it is a happy number or not """
+    total = 0
+    while (total != 4) and (total != 1):
+        total = 0
+        for numL in [int(a) for a in str(number)]:
+            total += numL ** 2
+        number = total
+        print(total)
+    if total == 1:
+        print("Is a happy number")
+    else:
+        print("Very sad number")
+        
+is_happy_number(input("Please input a positive integer: "))
